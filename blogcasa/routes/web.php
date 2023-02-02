fa5f93b67ccc4603a0cbcf18b76de02d5d68f51c
Marco <93841465+Marco1080@users.noreply.github.com>
2023-02-02 11:35:39 +0000

    Controllers added
    Added controllers in 'app>Controllers' to handle routes more cleanly and modulate

--- blogcasa/routes/web.php
+++ blogcasa/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CursoController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,27 +14,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', HomeController::class);
 
-Route::get('cursos', function () {
-    return "Bienvenido a la página";
-});
+Route::get('cursos', [CursoController::class, 'index']);
 
-Route::get('cursos/create', function () {
-    return "En esta página puedes crear un curso.";
-});
-/* 
-Route::get('cursos/{curso}', function($curso) {
-    return "Bienvenido al curso: $curso";
-}); */
+Route::get('cursos/create', [CursoController::class, 'create']);
 
-Route::get('cursos/{curso}/{categoria?}', function ($curso, $categoria = null) {
+
+Route::get('cursos/{curso}', [CursoController::class, 'show']);
+
+/* Route::get('cursos/{curso}/{categoria?}', function ($curso, $categoria = null) {
     if($categoria != null) {
         return "Bienvenido al curso $curso, de la categoria $categoria";
     }
     else{
         return "Bienvenido al curso $curso";
     }
-});
+}); */
